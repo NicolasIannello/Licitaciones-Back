4dc375730771a4f2c635eef892509d16c2315cbc
NicolasIannello <nicolas.iannello99@gmail.com>
2024-04-08 23:14:58 -0300

    ruta vehiculos imagen

--- index.js
+++ index.js
@@ -11,12 +11,8 @@ app.use( express.json() );
 
 dbConnection();
 
-app.use('/api/usuarios', require('./routes/usuarios'));
-app.use('/api/hospitales', require('./routes/hospitales'));
-app.use('/api/medicos', require('./routes/medicos'));
-app.use('/api/login', require('./routes/auth'));
-app.use('/api/todo', require('./routes/todo'));
-app.use('/api/upload', require('./routes/upload'));
+app.use('/api/vehiculos', require('./routes/vehiculos'));
+app.use('/api/imgs', require('./routes/imgs'));
 
 app.listen( process.env.PORT, () =>{
     console.log('Iniciando');
