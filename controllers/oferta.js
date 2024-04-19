968d1536e0cfe8d1193eeb6e2ece8faf60dcf1ca
NicolasIannello <nicolas.iannello99@gmail.com>
2024-04-19 19:55:56 -0300

    +marcarOferta

--- controllers/oferta.js
+++ controllers/oferta.js
@@ -1,6 +1,7 @@
 const { response }=require('express');
 const Vehiculo=require('../models/vehiculo');
 const Oferta=require('../models/oferta');
+const Vista = require('../models/vista');
 
 const getOfertas= async(req,res = response) =>{
     const matricula=req.query.matricula;
@@ -43,4 +44,26 @@ const crearOferta= async(req,res = response) =>{
     }
 };
 
-module.exports={getOfertas, crearOferta}
+const marcarOferta= async(req,res = response) =>{
+    const {user,matricula}=req.body;
+    
+    try {
+        const existeVista= await Vista.findOne({ $and: [ { 'matricula': { $eq: matricula } }, { 'user': { $eq: user } } ] });
+        
+        if(!existeVista){
+            const vista = new Vista(req.body)
+            await vista.save();     
+        }     
+
+        res.json({
+            ok:true,
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            ok:false,
+        });
+    }
+};
+
+module.exports={getOfertas, crearOferta, marcarOferta}
