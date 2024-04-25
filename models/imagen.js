19718b440175109ede4db608a65fb68aa40cbc64
NicolasIannello <nicolas.iannello99@gmail.com>
2024-04-25 17:01:54 -0300

    -unique

--- models/imagen.js
+++ models/imagen.js
@@ -3,7 +3,7 @@ const { Schema, model } = require('mongoose');
 const ImagenSchema = Schema({
     //matricula: { type: Schema.Types.ObjectId, require:true ,ref: 'Vehiculo' },
     matricula: { type: String, require:true},
-    img: { type: String, require:true, unique:true }
+    img: { type: String, require:true, /*unique:true*/ }
 });
 
 ImagenSchema.method('toJSON', function() {
