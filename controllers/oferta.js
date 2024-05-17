7bcf4938fcc6928f3081a93224bd74085872522e
NicolasIannello <nicolas.iannello99@gmail.com>
2024-05-17 20:42:13 -0300

    get oferta user / save to pdf

--- controllers/oferta.js
+++ controllers/oferta.js
@@ -2,18 +2,31 @@ const { response }=require('express');
 const Vehiculo=require('../models/vehiculo');
 const Oferta=require('../models/oferta');
 const Vista = require('../models/vista');
+const PDF = require('../models/pdf');
 
 const getOfertas= async(req,res = response) =>{
     const matricula=req.query.matricula;
+    const user=req.query.user;
 
-    const ofertasDB= await Oferta.find({ 'matricula': { $eq: matricula } },).sort({ oferta: -1 });
-    let cantidad= ofertasDB.length;
-    
-    res.json({
-        ok:true,
-        ofertasDB,
-        cantidad,
-    })
+    if(user==undefined){
+        const ofertasDB= await Oferta.find({ 'matricula': { $eq: matricula } },).sort({ oferta: -1 });
+        let cantidad= ofertasDB.length;
+
+        res.json({
+            ok:true,
+            ofertasDB,
+            cantidad,
+        })
+    }else{
+        const ofertasDB= await Oferta.find({ 'user': { $eq: user } },).sort({ oferta: -1 });
+        let cantidad= ofertasDB.length;
+
+        res.json({
+            ok:true,
+            ofertasDB,
+            cantidad,
+        })
+    }
 };
 
 const crearOferta= async(req,res = response) =>{
@@ -28,7 +41,9 @@ const crearOferta= async(req,res = response) =>{
         }
 
         const oferta = new Oferta(req.body);
+        const excel = new PDF(req.body);
         await oferta.save();
+        await excel.save();
 
         res.json({
             ok:true,
